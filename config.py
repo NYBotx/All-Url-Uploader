49c5cf04c0de4e87275e397250afbd0a810e4c66
NYBotx <167092531+NYBotx@users.noreply.github.com>
2025-01-06 05:32:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,27 +13,50 @@ load_dotenv()
 
 class Config(object):
     # Get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    # The Telegram API things
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    # Get these values from my.telegram.org
-    # Array to store users who are authorized to use the bot
-
-    # File /video download location
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8120897740:AAHBzDf484aQZwOlz4Sp5s7rwfHAMyU0JNM")
+    
+    API_ID = int(os.environ.get("API_ID", "13963336"))
+    
+    API_HASH = os.environ.get("API_HASH", "a144d1e22ef0b29738e8c00713d02678")
+    
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-
-    # Telegram maximum file upload size
-    TG_MAX_FILE_SIZE = 4194304000
-
-    # Chunk size that should be used with requests : default is 128KB
+    
+    MAX_FILE_SIZE = 2097152000
+    
+    TG_MAX_FILE_SIZE = 2097152000
+    
+    FREE_USER_MAX_FILE_SIZE = 2097152000
+    
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # Proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    
+    OUO_IO_API_KEY = ""
+    
+    MAX_MESSAGE_LENGTH = 4096
+    
+    PROCESS_MAX_TIMEOUT = 0
+    
+    DEF_WATER_MARK_FILE = "Url_uploader_nybot"
+    
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Url_uploader_nybot")
+    
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002421238378"))
+    
+    LOGGER = logging
 
-    # Set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3700
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002465691872")
+    
+    OWNER_ID = int(os.environ.get("OWNER_ID", "2103299862"))
+    
+    TG_MIN_FILE_SIZE = 2097152000
+    
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Url_uploader_nybot")
+                                  
 
     OWNER_ID = os.environ.get("OWNER_ID")
     ADL_BOT_RQ = {}
